89c65d3b45b41fc6dc3b2b349e5b6410ca8debe5
Marcelo Santos <marcelsud@gmail.com>
2022-06-17 00:11:36 +0000

    added tests for singleton and createContainer

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,34 @@
 type Store = Map<string, Callable>;
-export type Callable = (container: Container) => any
-
-export interface ProviderInterface {
-    register(c: Container): void
-}
+type Callable = (container: Container) => any;
 
 export class Container {
-    constructor(private store: Store = new Map()) {}
+  constructor(private store: Store = new Map()) {}
 
-    register(provider: ProviderInterface) {
-        provider.register(this)
+  set(id: string, callback: Callable | any): void {
+    this.store.set(id, callback);
+  }
+
+  get<T>(id: string): T {
+    if (!this.store.has(id)) {
+      throw new Error(`Service ${id} not found`);
     }
 
-    set(id: string, callback: Callable | any): void {
-        this.store.set(id, callback)
-    }    
+    let entry = this.store.get(id);
 
-    get<T>(id: string): T {
-        if (!this.store.has(id)) {
-            throw new Error(`Service ${id} not found`)
-        }
-
-        let entry = this.store.get(id)
-
-        if (entry === undefined || entry === null) {
-            throw new Error(`Service ${id} not found`)
-        }
-
-        if (typeof entry === "function") {
-            return entry(this)
-        }
-
-        return entry
+    if (entry === undefined || entry === null) {
+      throw new Error(`Service ${id} not found`);
     }
+
+    if (typeof entry === "function") {
+      return entry(this);
+    }
+
+    return entry;
+  }
+}
+
+export const container = new Container();
+
+export function createContainer() {
+  return new Container();
 }
---
+++ test/create-container.spec.ts
@@ -0,0 +1,44 @@
+import { createContainer } from "../src/index";
+
+const container = createContainer();
+
+const dateTimeParser = (timestamp: string): Date => {
+  return new Date(Date.parse(timestamp));
+};
+
+const toUpperCase = (a: string): string => {
+  return a.toUpperCase();
+};
+
+const toLowerCase = (a: string): string => {
+  return a.toLowerCase();
+};
+
+beforeEach(() => {
+  container.set("date_time_parser", () => dateTimeParser);
+  container.set("upper_case_transformer", () => toUpperCase);
+  container.set("lower_case_transformer", () => toLowerCase);
+});
+
+describe("All the tests needed and more", () => {
+  it("it should add a parameter properly", () => {
+    container.set("dummy_secret_key", "something");
+    let value = container.get("dummy_secret_key");
+    expect(value == "something");
+  });
+
+  it("it should add a simple service properly", async () => {
+    const toUpperCase = container.get<Function>("upper_case_transformer");
+    expect(toUpperCase("test")).toBe("TEST");
+
+    const toLowerCase = container.get<Function>("lower_case_transformer");
+    expect(toLowerCase("TEST")).toBe("test");
+  });
+
+  it("should work properly when using the container creation function", () => {
+    const parse = container.get<Function>("date_time_parser");
+    const date = parse("2022-06-15");
+    expect(date).toBeInstanceOf(Date);
+    expect(date.toISOString()).toEqual("2022-06-15T00:00:00.000Z");
+  });
+});
